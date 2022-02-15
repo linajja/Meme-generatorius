e568167eeed28f41f7b5ea1848073c8eecb87aa0
linajja <lina@netmarkuab.com>
2022-02-15 15:38:26 +0200

    meme

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+let inputTop = document.getElementsByClassName("input-top");
+let inputBot = document.getElementsByClassName("input-bot");
+// let btn = document.getElementsByClassName("btn");
+
+// btn.addEventListener("click", function (e) {
+let topText = inputTop.text;
+let botText = inputBot.text;
+console.log(topText);
+console.log(botText);
+//     document.getElementsByClassName("text-up").innerHtml = topText;
+//     document.getElementsByClassName("text-bot").innerHtml = botText;
+// });
