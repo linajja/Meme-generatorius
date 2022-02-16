58fc3e14f6f345eaddcabea03a68565628173e7a
Lina <94914937+linajja@users.noreply.github.com>
2022-02-16 14:54:50 +0200

    memo
    memo

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
+    let forma =document.forms["forma"];
 
-let inputTop = document.getElementsByClassName("input-top");
-let inputBot = document.getElementsByClassName("input-bot");
-// let btn = document.getElementsByClassName("btn");
+forma.addEventListener("submit", function(e){
+    e.preventDefault();
+    let inputTop = document.getElementById("text-up");
+    let inputBot = document.getElementById("text-bot");
+    let img =document.getElementById("img");
+   inputTop.innerHTML=forma["virsutinis"].value;
+inputBot.innerHTML=forma["apatinis"].value;
+img.setAttribute("src", forma["url"].value);
+});
 
-// btn.addEventListener("click", function (e) {
-let topText = inputTop.text;
-let botText = inputBot.text;
-console.log(topText);
-console.log(botText);
-//     document.getElementsByClassName("text-up").innerHtml = topText;
-//     document.getElementsByClassName("text-bot").innerHtml = botText;
-// });
+
